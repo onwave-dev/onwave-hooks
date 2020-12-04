074c280b464d086dd3a8428f82a90a34def150d4
kang-heewon <hw2656110>
2020-12-05 00:52:24 +0900

    chore: deploy v1.1.7

--- src/hooks/useSelect/index.ts
+++ src/hooks/useSelect/index.ts
@@ -1,55 +1,20 @@
 import React, { useState } from "react";
 
-type Params = {
-  list: any[];
-  placeHolder: string;
-  text?: any[];
-  initialIndex?: number;
-  isMultiple?: boolean;
-};
+export const useSelect = (
+  items: { value: number; text: string }[],
+  initialValue?: number,
+  placeHolder?: string
+) => {
+  const [value, setValue] = useState(initialValue);
 
-export const useSelect = ({
-  list,
-  text,
-  initialIndex,
-  isMultiple,
-  placeHolder,
-}: Params) => {
-  const [selectedIndex, setSelectedIndex] = useState(
-    isMultiple ? [initialIndex] : initialIndex
-  );
-
-  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    if (isMultiple) {
-      setSelectedIndex((prev: any[]) => [
-        ...prev,
-        list.indexOf(event.target.value),
-      ]);
-      return;
-    }
-    setSelectedIndex(list.indexOf(event.target.value));
-  };
-
-  const getMutipleValue = (indexs: (number | undefined)[]) => {
-    return list.filter((_, index) => indexs.includes(index));
-  };
-
-  const getValue = (index: number | (number | undefined)[] | undefined) => {
-    if (!index && index !== 0) {
-      return;
-    }
-    if (typeof index === "number") {
-      return list[index];
-    }
-    return getMutipleValue(index);
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setValue(Number(e.target.value));
   };
 
   return {
-    items: list,
+    value,
     onChange: handleChange,
-    value: getValue(selectedIndex),
-    text,
+    items,
     placeHolder,
-    multiple: isMultiple,
   };
 };
