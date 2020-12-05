1fd3f4dc11f27ba77658344ed8b817efa901b0ea
kang-heewon <heewon.dev@gmail.com>
2020-12-06 00:33:40 +0900

    chore: deploy v1.1.10

--- src/hooks/useInput/index.ts
+++ src/hooks/useInput/index.ts
@@ -2,14 +2,18 @@ import { useState } from "react";
 
 export const useInput = (initialValue: any) => {
   const [value, setValue] = useState(initialValue);
+  const [file, setFile] = useState<File>();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
+    if (e.target.files?.length && e.target.files[0]) {
+      setFile(e.target.files[0]);
+    }
   };
 
   const reset = () => {
     setValue(initialValue);
   };
 
-  return { value, onChange: handleChange, reset };
+  return { value, onChange: handleChange, reset, file };
 };
